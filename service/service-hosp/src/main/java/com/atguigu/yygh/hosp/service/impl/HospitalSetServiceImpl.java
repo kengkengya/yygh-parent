05a8e62c65b3d52db8577fc9c3f999bfdc44877a
kengkeng_glb <kengkeng_glb@qq.com>
2021-06-30 14:58:05 +0800

    第⑤次提交 整合hospital-manage 1.实现了医院模拟系统的的接口开发

--- service/service-hosp/src/main/java/com/atguigu/yygh/hosp/service/impl/HospitalSetServiceImpl.java
+++ service/service-hosp/src/main/java/com/atguigu/yygh/hosp/service/impl/HospitalSetServiceImpl.java
@@ -3,6 +3,7 @@ package com.atguigu.yygh.hosp.service.impl;
 import com.atguigu.yygh.hosp.mapper.HospitalSetMapper;
 import com.atguigu.yygh.hosp.service.HospitalSetService;
 import com.atguigu.yygh.model.hosp.HospitalSet;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -10,7 +11,13 @@ import org.springframework.stereotype.Service;
 @Service
 public class HospitalSetServiceImpl extends ServiceImpl<HospitalSetMapper, HospitalSet> implements HospitalSetService {
 
-    @Autowired
-    private HospitalSetMapper hospitalSetMapper;
 
+    @Override
+    public String getSignKey(String hosCode) {
+
+        QueryWrapper<HospitalSet> queryWrapper = new QueryWrapper<>();
+        queryWrapper.eq("hoscode", hosCode);
+        HospitalSet hospitalSet = baseMapper.selectOne(queryWrapper);
+        return hospitalSet.getSignKey();
+    }
 }
